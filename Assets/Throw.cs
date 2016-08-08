1803ba60b6652b8d45b8e92d2e47b5848bc4bd02
Wilson Censon <ken_nson@yahoo.com>
2016-08-08 10:29:14 +0800

    initial commit

---
+++ Assets/Throw.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Throw : MonoBehaviour {
+
+	public Transform head;
+	public bool holding = false;
+	public float speed = 20.5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Interact(GameObject ball){
+	
+
+		if (holding == false) {
+			ball.transform.parent = head;
+			holding = true;
+		
+		} else {
+			ball.transform.GetComponent<Rigidbody>().velocity = head.forward * speed;
+		}
+	}
+}
